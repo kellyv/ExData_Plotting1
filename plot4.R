e75790a2275800c82d7e68f68aa4ce85f3dca5c6
Kelly Vincent <kpvincent@gmail.com>
2015-02-07 20:00:50 -0800

    Correcting fourth plot file for missing code

--- plot4.R
+++ plot4.R
@@ -4,13 +4,17 @@ plot3 <- function() {
   data <- cbind(data, datetimed)
   subdata <- data[data$datetimed>=strptime("2007-02-01 00:00:00","%Y-%m-%d %H:%M:%S") & data$datetimed<strptime("2007-02-03 00:00:00","%Y-%m-%d %H:%M:%S"),]
   png("plot4.png", width = 480, height = 480)
-  
-  plot(subdata$datetimed, subdata$Global_active_power, type="n", ylab="Global Active Power (kilowatts)", xlab="")
+  par(mfcol=(c(2,2)))
+  plot(subdata$datetimed, subdata$Global_active_power, type="n", ylab="Global Active Power", xlab="")
   lines(subdata$datetimed, subdata$Global_active_power)
   plot(subdata$datetime, subdata$Sub_metering_1, type="n", ylab="Energy sub metering", xlab="")
   lines(subdata$datetimed, subdata$Sub_metering_1, col=1)
   lines(subdata$datetimed, subdata$Sub_metering_2, col=2)
   lines(subdata$datetimed, subdata$Sub_metering_3, col=4)
-  legend("topright", legend=c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), lwd=1, col=c(1,2,4))
+  legend("topright", legend=c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), lwd=1, col=c(1,2,4), box.lwd=0)
+  plot(subdata$datetime, subdata$Voltage, type="n", ylab="Voltage", xlab="datetime")
+  lines(subdata$datetimed, subdata$Voltage, col=1)
+  plot(subdata$datetime, subdata$Global_reactive_power, type="n", ylab="Global_reactive_power", xlab="datetime")
+  lines(subdata$datetimed, subdata$Global_reactive_power, col=1)
   dev.off()
 }
